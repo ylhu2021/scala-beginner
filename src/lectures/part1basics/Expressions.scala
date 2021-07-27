727190c6e5fbfa2bc60922aa9c35989a9bcf5069
ylhu2021 <hyl.heather@gmail.com>
2021-07-27 14:00:12 +0200

    first commit

---
+++ src/lectures/part1basics/Expressions.scala
@@ -0,0 +1,71 @@
+package lectures.part1basics
+
+object Expressions extends App {
+
+  val x = 1 + 2 // EXPRESSION
+  println(x)
+
+  println(2 + 3 * 4)
+  // + - * / & | ^ << >> >>> (right shift with zero extension)
+
+  println(1 == x)
+  // == != > >= < <=
+
+  println(!(1 == x))
+  // ! && ||
+
+  var aVariable = 2
+  aVariable += 3 // also works with -= *= /= ..... side effects
+  println(aVariable)
+
+  // Instructions (DO - executed) vs Expressions (VALUE - evaluated)
+
+  // IF expression
+  val aCondition = true
+  val aConditionedValue = if(aCondition) 5 else 3 // IF EXPRESSION
+  println(aConditionedValue)
+  println(if(aCondition) 5 else 3)
+  println(1 + 3)
+
+  var i = 0
+  val aWhile = while (i < 10) {
+    println(i)
+    i += 1
+  }
+
+  // NEVER WRITE THIS AGAIN.
+
+  // EVERYTHING in Scala is an Expression!
+
+  val aWeirdValue = (aVariable = 3) // Unit === void
+  println(aWeirdValue)
+
+  // side effects: println(), whiles, reassigning
+
+  // Code blocks
+
+  val aCodeBlock = {
+    val y = 2
+    val z = y + 1
+
+    if (z > 2) "hello" else "goodbye"
+  }
+  // the value of the block is the value of its last expression.
+  // the code inside the code block is only visible inside the code block
+
+  // 1. difference between "hello world" vs println("hello world")?
+  // ANSWER: the first one is type of string, the second one is an expression which is a side effect.
+
+  val someValue = {
+    2 < 3
+  }
+  println(someValue)
+
+  val someOtherValue = {
+    if(someValue) 239 else 986
+    42
+  }
+  println(someOtherValue)
+
+
+}
---
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,30 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+
+  val x: Int = 42
+  println(x)
+
+  // VALS ARE IMMUTABLE ... cannot be reassigned
+
+  // COMPILER can infer types
+
+  val aString: String = "hello"
+  val anotherString = "goodbye"
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort: Short = 4613
+  val aLong: Long = 5273985273895237L
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 3.14
+
+  // variables
+  var aVariable: Int = 4
+
+  aVariable = 5 // side effects ... allow to see what the programme is going, like print the result.
+}
+
+// all vals and vars have types
+// compiler automatically infers types when ommited
